b515c32edb733c97ea6eed187a1538544b5da965
TamirAvraham <tamir.avrah@gmail.com>
2022-10-21 22:13:29 +0300

    fuck my life

--- src/app/pizza-service.service.ts
+++ src/app/pizza-service.service.ts
@@ -1,7 +1,7 @@
 import { Pizza } from './pizza/pizza';
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { map, Observable } from 'rxjs';
+import { catchError, map, Observable, tap } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -22,11 +22,12 @@ export class PizzaServiceService {
   }
   getAllPizzas(){
     let ret=this.http.get<Map<string,any>>(`${this.url}GetAllPizzas`,this.httpOptions)
-    ret.forEach((value)=>{value.forEach((v,k)=>{
-      console.log(`${k}:${v}`)
-    })
-
-    })
+    console.log(`${ret.pipe(tap(ogaboga=>console.log("got this shit")))}`);
+    ret.pipe(tap(ogaboga=>console.log("got this shit")));
+    ret.pipe(tap(pizzas=>pizzas.forEach((v,k)=>console.log(`${k}:${v}`))));
+    console.log("fuck my life they both sucked");
+    
+    
      
     
   }
